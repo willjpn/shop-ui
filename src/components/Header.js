c232c8332e02b0e642a36ab9f5e2844d11a3f1fd
willnorris12 <will.norris@iiksa.com>
2022-02-09 08:18:04 +0000

    feat: add flexbox for homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,56 +1,112 @@
-import {Fragment} from "react";
-import {Link} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
-import {logout} from "../actions/userActions";
+import {Fragment} from 'react'
+import {Box, Button, Container, Grid, IconButton, Typography} from "@mui/material";
+import {Settings} from "@mui/icons-material";
+
 
 const Header = () => {
-    const user = useSelector(state => state.user)
-    const {userInfo} = user
-
-    const dispatch = useDispatch()
-
-    const signOut = () => {
-        dispatch(logout())
-    }
-
     return (
-        <Fragment>
-            <button>
-                <Link to="/">Home</Link>
-            </button>
-            {userInfo._id ?
-                <Fragment>
-                    <button onClick={signOut}>
-                        <Link to="/">Logout</Link>
-                    </button>
-                    <button>
-                        <Link to="/user-profile">Profile</Link>
-                    </button>
-                    {userInfo.isAdmin &&
-                    <Fragment>
-                        <button>
-                            <Link to="/product-list">Webshop Products</Link>
-                        </button>
-                        <button>
-                            <Link to="/user-list">See Users</Link>
-                        </button>
-                        <button>
-                            <Link to="/order-list">See Orders</Link>
-                        </button>
-                    </Fragment>
-                    }
-                </Fragment>
-                :
-                <button>
-                    <Link to="/login">Login</Link>
-                </button>
-            }
-            <button>
-                <Link to="/basket">Basket</Link>
-            </button>
+        <Container>
+            <Grid container sx={{}}>
+                <Grid item xs={12} md={4} sx={{border: 1, borderWidth: 'thin'}}>
 
-        </Fragment>
+                </Grid>
+                <Grid item xs={12} md={4} sx={{
+                    border: 1,
+                    borderWidth: 'thin',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
+                    <Typography>Webshop</Typography>
+                </Grid>
+                <Grid item xs={12} md={4} sx={{
+                    border: 1, borderWidth: 'thin', display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
+                    <Button sx={{mx: 2, padding: 2}}>Admin</Button>
+                    <Button sx={{mx: 2, padding: 2}}>Account</Button>
+                    <Button sx={{mx: 2, padding: 2}}>Logout</Button>
+                </Grid>
+            </Grid>
+
+        </Container>
+
+
     )
 }
 
 export default Header
+
+
+// import {Fragment} from "react";
+// import {Link} from "react-router-dom";
+// import {useDispatch, useSelector} from "react-redux";
+// import {logout} from "../actions/userActions";
+// import {Box, AppBar, Toolbar, IconButton, Typography, Button} from "@mui/material";
+//
+// const Header = () => {
+//     const user = useSelector(state => state.user)
+//     const {userInfo} = user
+//
+//     const dispatch = useDispatch()
+//
+//     const signOut = () => {
+//         dispatch(logout())
+//     }
+//
+//
+//
+//     return (
+//         <Box sx={{flexGrow: 1}}>
+//             <AppBar position="static">
+//                 <Toolbar>
+//                     <IconButton
+//                         size="large"
+//                         edge="start"
+//                         color="inherit"
+//                         aria-label="menu"
+//                         sx={{mr: 2}}
+//                     >
+//                     </IconButton>
+//                     <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+//                         <Link to="/">Webshop</Link>
+//                     </Typography>
+//                     {userInfo._id ?
+//                         <Fragment>
+//                             <Button color="inherit" onClick={signOut}>
+//                                 <Link to="/">Logout</Link>
+//                             </Button>
+//                             <Button color="inherit">
+//                                 <Link to='/user-profile'>
+//                                     Profile
+//                                 </Link>
+//                             </Button>
+//                             {userInfo.isAdmin &&
+//                             <Fragment>
+//                                 <Button color="inherit">
+//                                     <Link to="/product-list">Webshop Products</Link>
+//                                 </Button>
+//                                 <Button color="inherit">
+//                                     <Link to="/user-list">See Users</Link>
+//                                 </Button>
+//                                 <Button color="inherit">
+//                                     <Link to="/order-list">See Orders</Link>
+//                                 </Button>
+//                             </Fragment>
+//                             }
+//                         </Fragment>
+//                         : <Button color="inherit">
+//                             <Link to="/login">Login</Link>
+//                         </Button>
+//                     }
+//                     <Button color="inherit">
+//                         <Link to="/basket">Basket</Link>
+//                     </Button>
+//                 </Toolbar>
+//             </AppBar>
+//         </Box>
+//     );
+// }
+//
+// export default Header
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import {useEffect} from "react";
 import Products from "./Products";
 import Basket from "./Basket";
 import {queryProducts} from "../actions/productActions";
+import {Button, Container, Grid} from "@mui/material";
 
 const Home = () => {
 
@@ -25,15 +26,54 @@ const Home = () => {
     }, [dispatch, query])
 
     return (
-        <Fragment>
-            <Header/>
-            <Basket/>
-            <h2>Home Page</h2>
-            <input placeholder="Search" value={query} onChange={e => setQuery(e.target.value)}/>
-            {loading ? <h2>Loading products...</h2> : error ? <h2>An error has occurred: {error}</h2> :
-                <Products products={products} count={count}/>
-            }
-        </Fragment>
+
+        <Grid container sx={{
+            alignItems: 'flex-start'
+        }}>
+            <Grid xs={10}>
+                <Grid>
+                    <Grid container
+                          sx={{height: '10vh', backgroundColor: 'lightsteelblue'}}>
+                        <Grid item xs={4} sx={{border: 1, borderWidth: 'thin'}}>EMPTY</Grid>
+                        <Grid item xs={4} sx={{
+                            border: 1,
+                            borderWidth: 'thin',
+                            justifyContent: 'center',
+                            display: 'flex',
+                            alignItems: 'center'
+                        }}>LOGO</Grid>
+                        <Grid item xs={4} sx={{
+                            border: 1,
+                            borderWidth: 'thin',
+                            justifyContent: 'center',
+                            display: 'flex',
+                            alignItems: 'center'
+                        }}>
+                            <Button>Admin</Button>
+                            <Button>Account</Button>
+                            <Button>Logout</Button>
+                        </Grid>
+                    </Grid>
+                    <Grid sx={{border: 1, borderWidth: 'thin', height: '25vh', backgroundColor: 'lightblue'}}>
+                        Search
+                    </Grid>
+                    <Grid sx={{border: 1, borderWidth: 'thin', height: '65vh', backgroundColor: 'lightpink'}}>
+                        Body
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid xs={2} sx={{border: 1, borderWidth: 'thin', height: '100vh', backgroundColor: 'lightgreen'}}>
+                Basket
+            </Grid>
+        </Grid>
+
+        // {/*<Header/>*/}
+        // {/*<Basket/>*/}
+        // {/*<h2>Home Page</h2>*/}
+        // {/*<input placeholder="Search" value={query} onChange={e => setQuery(e.target.value)}/>*/}
+        // {/*{loading ? <h2>Loading products...</h2> : error ? <h2>An error has occurred: {error}</h2> :*/}
+        // {/*    <Products products={products} count={count}/>*/}
+        // {/*}*/}
     )
 }
 
